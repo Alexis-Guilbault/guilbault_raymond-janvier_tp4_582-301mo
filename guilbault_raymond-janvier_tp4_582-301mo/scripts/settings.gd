2ab2e5cab494aed7628f46e5fcab89867a90ab8c
Alexis <alexisguilbault@outlook.com>
2025-11-30 16:26:44 -0500

    commencement

---
+++ guilbault_raymond-janvier_tp4_582-301mo/scripts/settings.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var difficulty = 1
+## Difficulty Settings
+# 1 = 4x4, 4 minutes
+# 2 = 8x8, 8 minutes
+# 3 = 12x12, 12 minutes
+# 4 = 16x16, 16 minutes
+# 5 = 20x20, 20 minutes
+var difficulty_setting = [1, 2, 3, 4, 5]
+
+var puzzle_image = 1
+## Puzzle Image Settings
+# 1 = ???
+# 2 = ???
+# 3 = ???
+var puzzle_image_setting = [1, 2, 3]
+
+var background_image = 1
+## Background Image Settings
+# 1 = ???
+# 2 = ???
+# 3 = ???
+var background_image_setting = [1, 2, 3]
+
+var music_choice = 1
+## Background Music Settings
+# 1 = Type A
+# 2 = Type B
+var music_setting = [1, 2]
