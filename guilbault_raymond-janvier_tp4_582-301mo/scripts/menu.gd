825c58364a6af0e388ee1f97a5251249fdcd5c33
Alexis <alexisguilbault@outlook.com>
2025-12-05 13:57:27 -0500

    Responsive

--- guilbault_raymond-janvier_tp4_582-301mo/scripts/menu.gd
+++ guilbault_raymond-janvier_tp4_582-301mo/scripts/menu.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func _input(input) -> void:
-	if input == "select" or input == "pause":
+	if Input.is_action_just_pressed("Select") or Input.is_action_just_pressed("Pause"):
 		get_tree().change_scene_to_file("res://scenes/pieces/settings.tscn")
